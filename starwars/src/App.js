70237b51a5984f11f5ffdbbc0784eaebcdf57872
Taylor Wise <taylor2julie@gmail.com>
2020-04-17 22:58:29 -0500

    I believe I have completed MVP.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import CharacterCard from "./components/Character";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+width: 80%;
+margin: auto;
+display: flex;
+
+flex-wrap: wrap;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [character, setCharacter] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+     axios.get("https://swapi.py4e.com/api/people/")
+      .then(response => {
+        setCharacter(response.data.results); 
+      })
+      .catch(error => {
+        console.log("Uh oh something went wrong.", error);
+      });
+    }, []);
+  
+    console.log(character)
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Wrapper>
+      {character.map(character => {
+        return <CharacterCard key={character.id} character={character} />
+      })};
+      </Wrapper>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+import { CardHeader, CardBody, CardText} from 'reactstrap';
+
+
+
+const Card = styled.div`
+border: 4px ridge orange;
+color: black;
+width: 200px;
+max-height: 300px;
+display: flex;
+margin: 2%;
+flex-direction: column;
+align-items: center;
+background: rgb(179, 179, 179, 0.5);
+`;
+
+const CharacterCard = ({character}) => {
+    
+    return (
+        <Card>
+        <CardHeader tag="h3">{character.name}</CardHeader>
+        <CardBody>
+          <CardText>Gender: {character.gender}</CardText>
+          <CardText>Skin Color: {character.skin_color}</CardText>
+          <CardText>Height: {character.height}</CardText>
+          <CardText>Hair Color: {character.hair_color}</CardText>
+          <CardText>Eye Color: {character.eye_color}</CardText>
+          <CardText>Mass: {character.mass}</CardText>
+        </CardBody>
+        </Card>
+    );
+}
+
+export default CharacterCard;
